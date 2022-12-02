143e0766a1c1862e35cbfeb7a0d4f4242ca2760f
kmlindgren <kmlindgren@students.unwsp.edu>
2022-12-01 20:41:20 -0600

    Create unit test

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void testGetContent() {
+		Greeting greeting = new Greeting(1, "Test");
+		assertEquals("Test", greeting.getContent());
+	}
+}
